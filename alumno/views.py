410caa58cb4cfe15816ccf02a8f24b7a8a53f8e0
EnriqueVelascoJi <velascoenrique396@gmail.com>
2021-01-08 14:00:17 -0600

    email

--- alumno/views.py
+++ alumno/views.py
@@ -63,5 +63,5 @@ def crear_alumno(request):
     serializer = AlumnoSerializer(data = request.data)
     if  serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, serializar.data['email'], status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

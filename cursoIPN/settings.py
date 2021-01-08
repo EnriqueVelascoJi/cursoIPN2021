ae069c038bf72f68deefac3518e92cea84137c86
EnriqueVelascoJi <velascoenrique396@gmai.com>
2021-01-07 18:07:11 -0600

    m13

--- cursoIPN/settings.py
+++ cursoIPN/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'y1-hpvgk2678nquzcksjm(2^_*!15s($%0m53sr=ywt*4l+jz_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    ##Needs apps
+    ##Needs appshe
     'rest_framework',
     #apps from the project
     'alumno',

e70b7f886181d194cb15530eb9ad9f8cdda53156
EnriqueVelascoJi <velascoenrique396@gmail.com>
2021-01-09 01:53:05 -0600

    email

--- cursoIPN/settings.py
+++ cursoIPN/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 import dj_database_url
 from decouple import config
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y1-hpvgk2678nquzcksjm(2^_*!15s($%0m53sr=ywt*4l+jz_'
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config(DEBUG)
 ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL=True
@@ -153,8 +152,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ##CORREOS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "cnivelsuperior@gmail.com"
-EMAIL_HOST_PASSWORD = "ipnuamunam"
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER ')
+EMAIL_HOST_PASSWORD = config('ipnuamunam')
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
